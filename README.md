47f72b6575af60b6c954c7fffa92d78fdb1e73dd
rashed <x.suwaidi@gmail.com>
2020-08-28 18:26:26 +0400

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# hello_world
+# writing_prompts
 
 A new Flutter project.
 
---
+++ android/app/src/main/kotlin/com/example/writing_prompts/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.writing_prompts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
